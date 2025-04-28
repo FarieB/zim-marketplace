40a806fe281193591dbf36e16d3692e6c976d985
Lameck-Mupikeni <lameck.mupikeni@gmail.com>
2025-04-28 17:12:30 +0200

    Install Routing

---
+++ frontend-web/src/App.js
@@ -0,0 +1,12 @@
+
+n App() {
+	  return (
+		      <div className="App">
+		        <h1>Welcome to Zim Marketplace í·¿ï¿½ï¿½</h1>
+		        <p>Buy and sell easily across Zimbabwe.</p>
+		      </div>
+		    );
+}
+
+export default App;
+
