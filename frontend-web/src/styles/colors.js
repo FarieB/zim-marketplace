9fb3ac5a18fd1134f2c8be53e6a259ff45ab6ef6
FarieB <133366471+FarieB@users.noreply.github.com>
2025-04-28 17:31:22 +0200

    Create colors.js

---
+++ frontend-web/src/styles/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  primary: '#006400',     // Dark Green
+  secondary: '#FFD700',   // Gold Yellow
+  accent: '#FF0000',      // Red
+  black: '#000000',
+  white: '#FFFFFF',
+};
+
+export default colors;
