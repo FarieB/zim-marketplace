da4e4d857525ab7f7ce066704fcd49a419e16ecd
FarieB <133366471+FarieB@users.noreply.github.com>
2025-04-28 17:26:41 +0200

    Update App.js

--- frontend-web/src/App.js
+++ frontend-web/src/App.js
@@ -1,8 +1,22 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import ItemDetails from "./pages/ItemDetails";
+import Auth from "./pages/Auth";
+import PostItem from "./pages/PostItem";
+import Profile from "./pages/Profile";
+
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome to Zim Marketplace 🇿🇼</h1>
-      <p>Buy and sell easily across Zimbabwe.</p>
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/categories" element={<Categories />} />
+        <Route path="/item/:id" element={<ItemDetails />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/post-item" element={<PostItem />} />
+        <Route path="/profile/:id" element={<Profile />} />
+      </Routes>
     </div>
   );
 }
